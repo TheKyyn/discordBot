79806cd071b5e5efb04805b84097b7b6480b8fe9
Maxime <maximeth@MacBookDeMaxime.local>
2024-01-20 19:10:09 +0100

    Added system

--- src/database.js
+++ src/database.js
@@ -1,11 +1,3 @@
-const mongoose = require("mongoose");
+const pendingDonations = new Map();
 
-const dbConnection = process.env.MONGO_URI;
-
-mongoose
-  .connect(dbConnection, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connecté à MongoDB Atlas."))
-  .catch((err) => console.error("Erreur de connexion à MongoDB Atlas", err));
+module.exports = { pendingDonations };
